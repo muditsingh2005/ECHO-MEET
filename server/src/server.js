fbde6c816c1ddaf92edafae47b0b1008892caa04
Mudit Singh <muditsingh6624@gmail.com>
2026-02-03 19:29:52 +0530

    first commit

---
+++ server/src/server.js
@@ -0,0 +1,17 @@
+import { app } from "./app.js";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config({ 
+    path: "../.env" 
+});
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+})
+.catch((err) => {
+    console.log("MongoDB connection error : ", err);
+});
