85bf4a6ec9d481c9921a49185c922ff02c467959
Mudit Singh <muditsingh6624@gmail.com>
2026-04-26 15:51:40 +0530

    cors

--- AI-SERVICE-ECHO-MEET/src/app.js
+++ AI-SERVICE-ECHO-MEET/src/app.js
@@ -53,8 +53,6 @@ app.use(
   }),
 );
 
-app.options("*", cors());
-
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(cookieParser());
